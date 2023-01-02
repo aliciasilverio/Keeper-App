dd4c3b0a531f89cb846f43488be23f9c260f629f
aliciasilverio <aliciaalicia2455@gmail.com>
2023-01-02 12:34:24 -0500

    part 2

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,11 +1,23 @@
 import React from "react";
-import Header from "./header";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
 
 function App() {
-return (
-    <Header />
-
-
-)};
+  return (
+    <div>
+      <Header />
+      {notes.map(noteItem => (
+        <Note
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      ))}
+      <Footer />
+    </div>
+  );
+}
 
 export default App;
