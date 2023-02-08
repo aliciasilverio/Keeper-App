17087a5d36413292c5ca49e126cce5a9a2867d75
aliciasilverio <aliciaalicia2455@gmail.com>
2023-02-08 15:46:39 -0500

    style changes

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -52,7 +52,7 @@ function CreateArea(props) {
           onClick={expand}
           onChange={handleChange}
           value={note.content}
-          placeholder="Take a note..."
+          placeholder="Make a note..."
           rows={isExpanded ? 3 : 1}
         />
         <Zoom in={isExpanded}>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer>
-      <p>Copyright ⓒ {year}</p>
+      <p>Copyright Alicia Silverio ⓒ {year}</p>
     </footer>
   );
 }
